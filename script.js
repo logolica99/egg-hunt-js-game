c7985692d8ffb00d9f817ea95afd3ac979405c9a
Jubaer Jami <jamijubaer@gmail.com>
2023-02-18 00:15:06 +0600

    added collision physics

--- script.js
+++ script.js
@@ -54,6 +54,18 @@ window.addEventListener("load", () => {
       }
       this.collisionX += this.speedX * this.speedModifier;
       this.collisionY += this.speedY * this.speedModifier;
+
+      //collisions  with obstacles
+      this.game.obstacles.forEach((obstacle) => {
+        let [collision, distance, sumOfRadii, dx, dy] =
+          this.game.checkCollision(this, obstacle);
+        if (collision) {
+          const unit_x = dx / distance;
+          const unit_y = dy / distance;
+          this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x;
+          this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y;
+        }
+      });
     }
   }
 
@@ -140,6 +152,14 @@ window.addEventListener("load", () => {
       this.player.draw(context);
       this.player.update();
       this.obstacles.forEach((obstacle) => obstacle.draw(context));
+    }
+    checkCollision(a, b) {
+      const dx = a.collisionX - b.collisionX;
+      const dy = a.collisionY - b.collisionY;
+      const distance = Math.hypot(dy, dx);
+      const sumOfRadii = a.collisionRadius + b.collisionRadius;
+
+      return [distance < sumOfRadii, distance, sumOfRadii, dx, dy];
     }
     init() {
       let attempts = 0;
