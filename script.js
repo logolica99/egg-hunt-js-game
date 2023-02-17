9c1c4b89ddfe1dac7d145ea763c44ea95b274969
Jubaer Jami <jamijubaer@gmail.com>
2023-02-18 00:44:12 +0600

    added animation to the charachter

--- script.js
+++ script.js
@@ -18,10 +18,30 @@ window.addEventListener("load", () => {
       this.speedY = 0;
       this.dx = 0;
       this.dy = 0;
-      this.speedModifier = 10;
+      this.speedModifier = 6;
+      this.spriteWidth = 255;
+      this.spriteHeight = 255;
+      this.width = this.spriteWidth;
+      this.height = this.spriteHeight;
+      this.spriteX;
+      this.spriteY;
+      this.frameX = 0;
+      this.frameY = 5;
+      this.image = document.getElementById("bull");
     }
 
     draw(context) {
+      context.drawImage(
+        this.image,
+        this.frameX * this.spriteHeight,
+        this.frameY * this.spriteHeight,
+        this.spriteWidth,
+        this.spriteHeight,
+        this.spriteX,
+        this.spriteY,
+        this.width,
+        this.height
+      );
       context.beginPath();
       context.arc(
         this.collisionX,
@@ -43,6 +63,22 @@ window.addEventListener("load", () => {
     update() {
       this.dx = this.game.mouse.x - this.collisionX;
       this.dy = this.game.mouse.y - this.collisionY;
+
+      //sprite animation
+      const angle = Math.atan2(this.dy, this.dx);
+      if (angle < -2.74 || angle > 2.74) this.frameY = 6;
+      else if (angle < -1.96) this.frameY = 7;
+      else if (angle < -1.17) this.frameY = 0;
+      else if (angle < -0.39) this.frameY = 1;
+      else if (angle < 0.39) this.frameY = 2;
+      else if (angle < 1.17) this.frameY = 3;
+      else if (angle < 1.96) this.frameY = 4;
+      else if (angle < 2.74) this.frameY = 5;
+
+      //   else if (angle < 2.74) this.frameY = 5;
+
+      //
+
       const distance = Math.hypot(this.dy, this.dx);
 
       if (distance > this.speedModifier) {
@@ -54,7 +90,8 @@ window.addEventListener("load", () => {
       }
       this.collisionX += this.speedX * this.speedModifier;
       this.collisionY += this.speedY * this.speedModifier;
-
+      this.spriteX = this.collisionX - this.width * 0.5;
+      this.spriteY = this.collisionY - this.height * 0.5 - 100;
       //collisions  with obstacles
       this.game.obstacles.forEach((obstacle) => {
         let [collision, distance, sumOfRadii, dx, dy] =
